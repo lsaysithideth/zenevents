b3e79617ae8e92c88641ab0e751334c264ac1235
Linda Saysithideth <lsaysith@gmail.com>
2021-05-09 01:43:06 -0400

    startHeroku

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET events page. */
+router.get('/events', function(req, res, next) {
+  res.render('events');
+});
+
+/* GET quiz page. */
+router.get('/zenquiz', function(req, res, next) {
+  console.log("URL PARAMS:", req.query)
+  var name = req.query.name || "World" // double pipes is an OR operator that allows us to use a default value if the url params are null / not specified
+  var message = "Hello, " + name
+  res.render('zenquiz', { message: message });
+});
+
 module.exports = router;
